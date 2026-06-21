7da8d80e732031bd0a8803b266845236392a5e25
Jagadish Narayan Samal <100665749+jnsamal@users.noreply.github.com>
2026-06-21 14:34:40 +0000

    Added a program on namespace

--- codes/test.cpp
+++ codes/test.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
+using namespace std;
 
+namespace first{
+    int x = 1;
+}
 
+namespace second{
+    int x = 2;
+}
 
 int main()
 {
 
+    int x = 0;
+
+    cout << x << '\n';
 
     return 0;
 }
