47f54c3ab73b0174a9a996df01c211bcd1e05736
Jagadish Narayan Samal <100665749+jnsamal@users.noreply.github.com>
2026-06-22 05:48:53 +0000

    Create a file on enum

---
+++ codes/2_enum.cpp
@@ -0,0 +1,16 @@
+// Enum a user defined data type which holds a collection of constant variables each having a predefined or specified value
+
+#include <bits/stdc++.h>
+using namespace std;
+
+enum Level{
+    LOW = 25,
+    MEDIUM = 50,
+    HIGH = 75
+};
+
+int main()
+{
+    Level l = LOW;
+    cout << l << '\n';
+}
